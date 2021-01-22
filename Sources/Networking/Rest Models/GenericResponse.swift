9ee6fd16020e7c884443caa9f639604e0a6f50ad
Bill Panagiotopoulos <billp.dev@gmail.com>
2021-01-22 17:15:36 +0200

    Update copyright text

--- Sources/Networking/Rest Models/GenericResponse.swift
+++ Sources/Networking/Rest Models/GenericResponse.swift
@@ -3,7 +3,7 @@
 //  Example-iOS
 //
 //  Created by Vasilis Panagiotopoulos on 16/9/20.
-//  Copyright © 2020 Ryo Aoyama. All rights reserved.
+//  Copyright © 2020 Vasilis Panagiotopoulos. All rights reserved.
 //
 
 import Foundation
